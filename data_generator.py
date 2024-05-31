7c519fa6252f6a45c6bfd6ec40414ad9a4d2c82a
YuliaTerzieva <yulia.terieva@gmail.com>
2024-05-31 18:42:57 +0200

    weak/strong plot

--- data_generator.py
+++ data_generator.py
@@ -34,7 +34,7 @@ def gen_paths_GBM(S_0 = 1, mu = 0.05, sigma = 0.2, dt = 0.05, n_steps = 100, n_p
     return S_E, S_M, Exact_solution, Z, Log_Return
 
 def gen_paths_from_GAN(gen_model, S_0, dt, n_steps, n_paths, actual_log_returns = None, Z_BM = None, my_device = 'mps'): 
-    if Z_BM !=None :
+    if Z_BM is not None :
         Z = Z_BM
     else:
         Z = torch.randn((n_steps, n_paths)).type(torch.FloatTensor).to(device=torch.device(my_device))
@@ -42,14 +42,14 @@ def gen_paths_from_GAN(gen_model, S_0, dt, n_steps, n_paths, actual_log_returns 
     G_paths = torch.zeros((n_steps, n_paths)).type(torch.FloatTensor).to(device=torch.device(my_device))
     G_paths[0, :] = S_0
 
-    if actual_log_returns.any() != None:
+    if actual_log_returns is not None:
         actual_log_returns = torch.from_numpy(actual_log_returns).type(torch.FloatTensor).to(device=torch.device(my_device))
 
     for step_inx in range(n_steps-1):
-        if actual_log_returns.any() != None :
+        if actual_log_returns is not None :
             input = actual_log_returns[step_inx].view(1, -1)
         else :
-            input = G_paths[step_inx]
+            input = G_paths[step_inx].view(1, -1)
         gen_pred = gen_model(Z[step_inx].view(1, -1), (input, c_dt)).T
         gen_pred = torch.exp(gen_pred)
         gen_pred = gen_pred * G_paths[step_inx]
@@ -63,30 +63,41 @@ if __name__ == '__main__':
     S_0 = 1
     mu = 0.05
     sigma = 0.2
-    n_steps_array = [50, 100, 200, 400, 500]
-    n_paths = 1_000
+    n_steps_array = [50, 100, 200, 400]
+    n_paths = 500
     dt = 0.05
 
-    error_Weak = np.zeros((len(n_steps_array)))
-    error_Strong = np.zeros((len(n_steps_array)))
+    error_Weak_one_step = np.zeros((len(n_steps_array)))
+    error_Strong_one_step = np.zeros((len(n_steps_array)))
+    error_Weak_self_gen = np.zeros((len(n_steps_array)))
+    error_Strong_self_gen = np.zeros((len(n_steps_array)))
+
     for i, n_steps in enumerate(n_steps_array):
         S_E, S_M, Exact_solution, Z, Log_Return = gen_paths_GBM(S_0, mu, sigma, dt, n_steps, n_paths)
+
         gen_model = torch.load(f'generator_{config_name}.pth')
         gen_model.eval()
-        model_paths = gen_paths_from_GAN(gen_model, S_0, dt, n_steps, n_paths, actual_log_returns = Log_Return)
-        error_Weak[i] = np.abs(np.mean(Exact_solution[-1])-np.mean(model_paths[-1]))
-        error_Strong[i] = np.mean(np.abs(Exact_solution[-1]-model_paths[-1]))
-        print(error_Weak[i], error_Strong[i])
 
-    plt.plot(n_steps_array, error_Weak, label = 'Weak')
-    plt.plot(n_steps_array, error_Strong, label = 'Strong')
+        model_paths_one_step = gen_paths_from_GAN(gen_model, S_0, dt, n_steps, n_paths, actual_log_returns=Log_Return)
+        error_Weak_one_step[i] = np.abs(np.mean(Exact_solution[-1])-np.mean(model_paths_one_step[-1]))
+        error_Strong_one_step[i] = np.mean(np.abs(Exact_solution[-1]-model_paths_one_step[-1]))
+
+        model_paths_self_gen =  gen_paths_from_GAN(gen_model, S_0, dt, n_steps, n_paths)
+        error_Weak_self_gen[i] = np.abs(np.mean(Exact_solution[-1])-np.mean(model_paths_self_gen[-1]))
+        error_Strong_self_gen[i] = np.mean(np.abs(Exact_solution[-1]-model_paths_self_gen[-1]))
+
+    plt.plot(n_steps_array, error_Weak_one_step, label = 'Weak error one step')
+    plt.plot(n_steps_array, error_Strong_one_step, label = 'Strong error one step')
+    plt.plot(n_steps_array, error_Weak_self_gen, label = 'Weak error self gen')
+    plt.plot(n_steps_array, error_Strong_self_gen, label = 'Strong error self gen')
     plt.legend()
-    plt.savefig(f"Weak_Stong {config_name}")
+    plt.savefig(f"Weak_Stong {config_name} all gened")
     plt.show()
 
-    # plt.plot(Exact_solution[:, :100], alpha = 0.7, color = 'lightblue')
-    # plt.plot([], [], color = 'lightblue', label = "Real")
-    # plt.plot(model_paths[:, :100], alpha = 0.5, color = 'palevioletred')
+    
+    # plt.plot(model_paths_self_refered[:, :100], alpha = 0.7, color = 'lightblue')
+    # plt.plot([], [], color = 'lightblue', label = "Self_reffered")
+    # plt.plot(model_paths_one_step[:, :100], alpha = 0.5, color = 'palevioletred')
     # plt.plot([], [], alpha = 0.5, color = 'palevioletred', label = "Generated one-step")
     # plt.title("Generated vs actual paths")
     # plt.legend()
--- plots.py
+++ plots.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from data_generator import *
 
 def losses(D_losses, G_losses, config_name):
     plt.figure()
@@ -29,3 +30,35 @@ def log_returns(actual_log_returns, model, n_steps, n_paths, batch_size, c_dt, c
     plt.legend()
     plt.title(f"Log Returns {config_name}")
     plt.savefig(f"Log Returns {config_name}")
+
+def weak_stong_error_gen_paths(config_name, S_0 = 1, mu = 0.05, sigma = 0.2, n_steps_array = [50, 100, 200, 400], n_paths = 500, dt = 0.05):
+    np.random.seed(42)
+
+    error_Weak_one_step = np.zeros((len(n_steps_array)))
+    error_Strong_one_step = np.zeros((len(n_steps_array)))
+    error_Weak_self_gen = np.zeros((len(n_steps_array)))
+    error_Strong_self_gen = np.zeros((len(n_steps_array)))
+
+    for i, n_steps in enumerate(n_steps_array):
+        S_E, S_M, Exact_solution, Z, Log_Return = gen_paths_GBM(S_0, mu, sigma, dt, n_steps, n_paths)
+
+        gen_model = torch.load(f'generator_{config_name}.pth')
+        gen_model.eval()
+
+        model_paths_one_step = gen_paths_from_GAN(gen_model, S_0, dt, n_steps, n_paths, actual_log_returns=Log_Return)
+        error_Weak_one_step[i] = np.abs(np.mean(Exact_solution[-1])-np.mean(model_paths_one_step[-1]))
+        error_Strong_one_step[i] = np.mean(np.abs(Exact_solution[-1]-model_paths_one_step[-1]))
+
+        model_paths_self_gen =  gen_paths_from_GAN(gen_model, S_0, dt, n_steps, n_paths)
+        error_Weak_self_gen[i] = np.abs(np.mean(Exact_solution[-1])-np.mean(model_paths_self_gen[-1]))
+        error_Strong_self_gen[i] = np.mean(np.abs(Exact_solution[-1]-model_paths_self_gen[-1]))
+
+    plt.plot(n_steps_array, error_Weak_one_step, linestyle='dashed', label = 'Weak error one step')
+    plt.plot(n_steps_array, error_Strong_one_step, linestyle='dashed', label = 'Strong error one step')
+    plt.plot(n_steps_array, error_Weak_self_gen, label = 'Weak error self gen')
+    plt.plot(n_steps_array, error_Strong_self_gen, label = 'Strong error self gen')
+    plt.xlabel("number of steps")
+    plt.title(f"Weak_Stong {config_name}")
+    plt.legend()
+    plt.savefig(f"Weak_Stong {config_name}")
+    plt.show()
